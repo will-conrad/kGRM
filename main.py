0a0adccf566352555a7250ccd7971c38aaf51e8e
Will Conrad <jwconrad23@gmail.com>
2025-11-23 01:26:20 -0500

    remove commented-out cat image code; add webcam capture functionality

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import cv2 as cv
 
-cat = cv.imread("media/cat.jpg")
-cv.imshow("CAT", cat)
-cv.waitKey(0)
+# cat = cv.imread("media/cat.jpg")
+# cv.imshow("CAT", cat)
+# cv.waitKey(0)
 
+cap = cv.VideoCapture(1)
+cap.set(cv.CAP_PROP_FRAME_WIDTH, 600)
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, 600)
 
+success, frame = cap.read()
+if success:
+    cv.imshow("Webcam", frame)
+    cv.waitKey(0)
