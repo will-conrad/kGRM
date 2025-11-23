c63d9033e7552e227b83619807b3df739ac824e9
Will Conrad <jwconrad23@gmail.com>
2025-11-23 01:30:50 -0500

    testing photos

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import cv2 as cv
 
+
+webcam_ID = 0
+
+# Show cat
 # cat = cv.imread("media/cat.jpg")
 # cv.imshow("CAT", cat)
 # cv.waitKey(0)
 
-cap = cv.VideoCapture(1)
+# Setup Capture Device
+cap = cv.VideoCapture(0)
 cap.set(cv.CAP_PROP_FRAME_WIDTH, 600)
 cap.set(cv.CAP_PROP_FRAME_HEIGHT, 600)
 
@@ -12,3 +17,14 @@ success, frame = cap.read()
 if success:
     cv.imshow("Webcam", frame)
     cv.waitKey(0)
+    cap.release()
+
+# Save frame
+def take_photo():
+    print("Taking Photo")
+    cap = cv.VideoCapture(webcam_ID)
+    success, frame = cap.read()
+    cv.imwrite('photos/test.jpg', frame)
+    cap.release()
+
+take_photo()
